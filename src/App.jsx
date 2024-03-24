d97f8d2772a7383aba42cb56987297ffad6124a5
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-03-25 02:20:46 +0530

    Added a IPhone Modal with Three JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
+import Model from './components/Model';
 
 function App() {
-  return(
+  return (
     <main className="bg-black">
       <Navbar />
-      <Hero/>
-      <Highlights/>
+      <Hero />
+      <Highlights />
+      <Model />
     </main>
-  )
+  );
 }
 
 export default App;
