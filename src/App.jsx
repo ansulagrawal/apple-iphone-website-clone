15118e4ae4f72cae17c2744e71dce45f9d24cc5a
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-03-25 02:28:06 +0530

    Added Vercel Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
@@ -5,12 +6,15 @@ import Model from './components/Model';
 
 function App() {
   return (
-    <main className="bg-black">
-      <Navbar />
-      <Hero />
-      <Highlights />
-      <Model />
-    </main>
+    <>
+      <main className="bg-black">
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
+      </main>
+      <SpeedInsights />
+    </>
   );
 }
 
